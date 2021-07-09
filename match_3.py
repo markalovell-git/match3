9d123c3542b5dd1180966ac4e5b2ea6ba5bf9a2b
markalovell-git <mark@trueface.ai>
2021-07-09 16:01:49 -0400

    got numpy working... a bit

--- match_3.py
+++ match_3.py
@@ -7,27 +7,15 @@ import numpy as np
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Match Three!"
+NUM_OF_GEMS = 6 # can't be greater than 7 unless I make new graphics
+
+COLS = 3
+ROWS = 5
 
 class MyGame(arcade.Window):
     """
     Main application class.
     """
-    # mouse_press_x = 0
-    # mouse_press_y = 0
-    # mouse_release_x = 0
-    # mouse_release_y = 0
-
-    # drop_jitter = .90
-    # drop_height = SCREEN_HEIGHT
-
-    # gem_to_move = Gem
-    # gem_being_moved = Gem
-    # destroy_gem_index = 0
-    
-    # game_state = "booting"
-    # switch_free_range_mode = False
-    
-    # PAUSE = False
 
     def __init__(self, width, height, title):
         """
@@ -39,45 +27,75 @@ class MyGame(arcade.Window):
 
         # If you have sprite lists, you should create them here,
         # and set them to None
-        self.gem_list = None
-        self.special_gem_list = None
-        self.foreground_gem_list = None
-        self.icons_list = None
+        # self.gem_list = None
 
         # Declare Variables
-        self.gem_sprite = None
-        self.number_grid = None
+        # self.gem_sprite = None
+
 
         # Set the background color
         arcade.set_background_color(arcade.color.BLACK)
 
         # # Don't show the mouse cursor
         # self.set_mouse_visible(False)
+        
     def setup(self):
         """ Set up the game and initialize the variables. """
-
-        self.drop_jitter = .90
-        self.drop_height = SCREEN_HEIGHT
-
-        self.game_state = "initializing"
-        self.game_state_temp = ""
-        self.switch_free_range_mode = False
-
-        self.wait_timer = 0
-        self.moves_remain_list = []
     
         # Sprite lists
         self.gem_list = arcade.SpriteList()
-        self.special_gem_list = arcade.SpriteList()
-        self.foreground_gem_list = arcade.SpriteList()
-        self.icons_list = arcade.SpriteList()
+
+class NumberGrid:
+    """
+    The mathematical numbergrid that underlies the display
+    """
+
+    def __init__(self):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__()
+    
+    def init_number_grid():
+        # ng = np.empty_like(shape( object, dtype=object)
+        ng = np.empty((ROWS, COLS), dtype=object)
+        ng.fill([0, "No Gem"])
+
+        return ng
+
+    def output_number_grid(ng):
+        for i in range(ROWS):
+            print("Row {}: ".format(i))
+            print_row = ""
+            for j in range(COLS):  
+                print_row += str(ng[i][j])
+            print(print_row + "\n")
+
+    # def make_list_of_gems():
+    #     total_gem_count = ROWS * COLS
+    #     gem_list = []
+    #     for i in range (total_gem_count):
+    #         gem_list.append([0, "two"])
+    #     return gem_list
+
+
+    number_grid = init_number_grid()
+
+    # populate()
+    number_grid[4][2] = [1, "3gem"]
+
+    output_number_grid(number_grid)
+
+number_grid = NumberGrid(number_grid)
+print(number_grid[3][2])
+   
 
 def main():
     """ Main method """
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
-
-
 if __name__ == "__main__":
     main()
