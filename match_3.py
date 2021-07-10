378cc1dcfd88cc0e38c8a21b3848482dd9fc2fcf
markalovell-git <mark@trueface.ai>
2021-07-10 18:20:01 -0400

    match detect working + changed number_grid to dict

--- match_3.py
+++ match_3.py
@@ -57,67 +57,75 @@ class NumberGrid:
         """
 
         self.grid = np.empty((ROWS, COLS), dtype=object)
-        self.grid.fill([0, "No Gem"])
+        self.grid.fill({
+            "color" : 0,
+            "type"  : "No Gem",
+            "is matched" : False
+        })
+
         self.number_list = [] # random list of numbers that feed into the number grid
 
     def print_out_number_grid(self):
         print ("*" * 80)
-        for i in range(ROWS):
+        for i in range(ROWS): # display bottom row first, easier for thinking in xy coords
             # print("Row {}: ".format(i))
             print_row = ""
             for j in range(COLS):  
                 # print_row += str(self.grid[i][j]) 
-                print_row += (str(self.grid[i][j][0]) + "  ")  
+                print_row += str(self.grid[i][j].get("color")) + "  "
             print(print_row + "\n")
         print ("*" * 80)
 
     def generate_new_number_list(self):
+        random_number_list = []
         total_number_of_items_in_number_grid = np.count_nonzero(self.grid)
         for i in range(total_number_of_items_in_number_grid):
             random_number =  random.randint(1, NUM_OF_GEMS)
-            self.number_list.append(random_number)
+            random_number_list.append(random_number)
+            # print(random_number_list)
+        return(random_number_list)
             
-    def grid_has_matches(self):
-        for i in range(ROWS):
-            for j in range(COLS):
+    def check_grid_for_matches(self):
+        for i in range(2, ROWS):
+            for j in range(2, COLS):
                 # horizontal check
-                try:
-                    this_number = self.grid[i][j][0]
-                    one_to_the_left = self.grid[i][j-1][0]
-                    two_to_the_left = self.grid[i][j-2][0]
-                except:
-                    pass
-                # vertical check
+                this_number = self.grid[i][j].get("color")
+                one_to_the_left = self.grid[i][j-1].get("color")
+                two_to_the_left = self.grid[i][j-2].get("color")
+                one_above = self.grid[i-1][j].get("color")
+                two_above = self.grid[i-2][j].get("color")
+
+                # reverse the order of rows so that top row becomes bottom row  
+                # making it easier to think in terms of x, y instead of x, -y 
+                display_i = abs(i - ROWS) - 1
+                if this_number == one_to_the_left == two_to_the_left:
+                    print("Horizontal Match at {}, {}.".format(j, display_i))
+                if this_number == one_above == two_above:
+                    print("Vertical Match at {}, {}.".format(j,display_i))
+
     def populate_grid_with (self, number_list):
         for i in range(ROWS):
             for j in range(COLS): 
                 this_number = number_list.pop(0)
-                data = [this_number, "3Gem"]
+                data = {
+                    "color" : this_number,
+                    "type"  : "3 Gem",
+                    "is matched" : False
+                }   
                 self.grid[i][j] = data
 
-    def change_number_grid_xy(self, row, col, arg):
-        grid = self.grid
-        grid[col-1,row-1] = [2, "gem"]
-
-    # number_grid = init_number_grid()
-
-    # # populate()
-    # number_grid[4][2] = [1, "3gem"]
-
-    # output_number_grid(number_grid)
-
 number_grid = NumberGrid()
-number_grid.generate_new_number_list()
-
-# print(number_grid.number_list)
-number_grid.populate_grid_with(number_grid.number_list)
+random_number_list = number_grid.generate_new_number_list()
+number_grid.populate_grid_with(random_number_list)
 
 number_grid.print_out_number_grid()   
 
-def main():
-    """ Main method """
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    window.setup()
-    arcade.run()
-if __name__ == "__main__":
-    main()
+number_grid.check_grid_for_matches()
+
+# def main():
+#     """ Main method """
+#     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+#     window.setup()
+#     arcade.run()
+# if __name__ == "__main__":
+#     main()
